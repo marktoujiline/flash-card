375865e5103fc20ff6f6c6f7634f3b7c5b97d153
Mark Toujiline <mark.toujiline@gmail.com>
2019-11-16 22:15:05 -0800

    Refactor flashcard to use hooks

--- components/Flashcard.js
+++ components/Flashcard.js
@@ -1,21 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Animated, Easing, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 
-export default class Flashcard extends React.Component {
+const Flashcard = ({front, back}) => {
+    const [currentSide, setCurrentSide] = useState('front');
+    const [transform, setTransform] = useState([]);
 
-  constructor(props) {
-    super(props);
-    this.onButtonPressed = this.onButtonPressed.bind(this);
-    this.changeSide = this.changeSide.bind(this);
-    this.state = {
-      currentSide : "front",
-      transform: []};
-  }
-
-  changeSide() {
-    this.setState({
-      currentSide: (this.state.currentSide === "front") ? "back" : "front"
-    });
+  const changeSide = () => {
+    setCurrentSide(currentSide === "front" ? "back" : "front");
     let rotateXVal = new Animated.Value(90);
     Animated.timing( rotateXVal, {
       toValue: 0,
@@ -28,11 +19,10 @@ export default class Flashcard extends React.Component {
       outputRange: ['360deg', '0deg'],
     });
 
-    this.setState({
-      transform: [{ rotateX: x}]});
+    setTransform([{ rotateX: x}]);
   }
 
-  animate() {
+  const animate = () => {
     let rotateXVal = new Animated.Value(0);
     //let rotateZVal = new Animated.Value(0);
 
@@ -40,43 +30,38 @@ export default class Flashcard extends React.Component {
       toValue: 90,
       duration:200,
       easing: Easing.linear
-    }).start(() => this.changeSide());
+    }).start(() => changeSide());
 
     let x = rotateXVal.interpolate({
       inputRange: [0, 360],
       outputRange: ['0deg', '360deg'],
     });
 
-    this.setState({
-      transform: [{ rotateX: x}]});
+    setTransform([{ rotateX: x}]);
   }
 
-  onButtonPressed() {
-    this.animate();
-  }
-
-  render() {
-    let side = (this.state.currentSide === "front") ? this.props.front : this.props.back;
+    let side = (currentSide === "front") ? front : back;
 
     const styles = StyleSheet.create({
-      container: {
+        container: {
         height:250,
         width:450,
         backgroundColor: 'transparent',
         alignItems: 'center',
         justifyContent: 'center',
         borderWidth: 1,
-        transform:this.state.transform
-      },
+        transform
+        },
     });
 
     return (
       <TouchableWithoutFeedback
-        onPress={this.onButtonPressed}>
+        onPress={() => animate()}>
         <Animated.View style={styles.container}>
           {side}
         </Animated.View>
       </TouchableWithoutFeedback>
     );
-  }
 }
+
+export default Flashcard;
